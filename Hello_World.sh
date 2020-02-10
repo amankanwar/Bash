a3c5a5967513e2cfe764e20cfabf1b59a89ad7a6
Aman Kanwar <iamamankanwar@gmail.com>
2020-02-10 23:09:11 +0530

    Add files via upload

---
+++ Hello_World.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Author - Aman Kanwar, dated Feb 10th, 2020
+
+# This is my first try of the bash programming
+# There are few points which I would like to point out
+# the command on the top is called as sheband1 this command
+# sheband1 instructs the operating system to run the bash from
+# the path /bin/bash
+#
+# furthermore, the file we have created can be executed using the command
+# ./Hello_World.sh
+# however, we will see a warning related to permission, if we try the same
+# In order to execute the given file, we can either do the same eithe by
+# using the following, $- bash Hello_World.sh
+# or by using the      $- sh   Hello_World.sh
+
+
+#
+# In order to execute the same with the normal command, ./Hello_World.sh
+# we need to provide permissinos to the given file using the chmod
+# I will go throuh it later on
+# in order to provide the executible permission.
+
+
+# execute the following command before running the script
+# $- chmod +x Hello_World.sh
+
+
+echo "Hello Aman this is the first try of bash"
+
+
